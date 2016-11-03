ae0f3306015c82a403a4d2c2d89eac0b2c41084b
Scott Donaldson <scott.p.donaldson@gmail.com>
2016-11-03 10:03:05 -0400

    cache busting

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 
 <?php wp_footer(); ?>
 
-<script type="text/javascript" src="<?php echo bloginfo('template_url'); ?>/js/script.js"></script>
+<script type="text/javascript" src="<?php echo bloginfo('template_url'); ?>/js/script.js?v=20161103_1002"></script>
 
 </body>
 </html>
