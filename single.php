5aeed2119ffa3c26619796c73692cbfecd5062ff
Scott Donaldson <scott.p.donaldson@gmail.com>
2016-11-15 11:07:13 -0500

    move postmeta

--- single.php
+++ single.php
@@ -3,22 +3,28 @@
 
 <h2><?php the_title(); ?></h2>
 
-<?php the_content(); ?>
-
-<div>
-Author: <a href="<?php echo get_author_link(false, $authordata->ID, $authordata->user_nicename) ?>"> <?php the_author_meta('first_name'); echo ' '; the_author_meta('last_name'); ?> </a> <br />
-Category: <?php echo get_the_category_list(', ')?>
+<div class="postmeta">
+<?php the_date('F Y'); ?><br>
+Author: <a href="<?php echo get_author_link(false, $authordata->ID, $authordata->user_nicename) ?>"> <?php the_author_meta('first_name'); echo ' '; the_author_meta('last_name'); ?></a>
+<?php if (get_field('program', 'user_' . $authordata->ID)) {
+	echo ', ' . get_field('program', 'user_' . $authordata->ID);
+	if (get_field('graduation_year', 'user_' . $authordata->ID)) {
+		echo ' ' . get_field('graduation_year', 'user_' . $authordata->ID);
+	}
+} ?>
 </div>
+
+<?php the_content(); ?>
 
 <div class="clear"></div>
 
-			<?php endwhile; else : ?>
+<?php endwhile; else : ?>
 
-				<h2 class="center">Not Found</h2>
-				<p class="center">Sorry, but you are looking for something that isn't here.</p>
-				<?php include (TEMPLATEPATH . "/searchform.php"); ?>
+	<h2 class="center">Not Found</h2>
+	<p class="center">Sorry, but you are looking for something that isn't here.</p>
+	<?php include (TEMPLATEPATH . "/searchform.php"); ?>
 
-			<?php endif; ?>
+<?php endif; ?>
 
 
 <?php get_footer(); ?>
